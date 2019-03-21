2e5e7aa8a22c5388c43408053819cb9dfe6cb435
Andrew Ouellette <ouellette.andrew@gmail.com>
2019-03-21 13:21:56 -0500

    Clear Grid function added

--- designs.js
+++ designs.js
@@ -11,19 +11,22 @@ console.log($('#colorPicker').val());
 // Grid size button
 $('#gridSizeButton').on('click', function (event) {
   event.preventDefault();
+  clearGrid();
   const height = $('#inputHeight').val();
   const width = $('#inputWidth').val();
   makeGrid(height, width);
 });
 
 // When size is submitted by the user, call makeGrid()
-
 function makeGrid(height, width) {
   for (let h = 1; h <= height; h++) {
     $('#pixelCanvas').append('<tr>');
-    console.log('row' + h);
     for (let w = 1; w <= width; w++) {
       $('tr:last-child').append('<td>');
     }
   }
+}
+
+function clearGrid() {
+  $('#pixelCanvas').empty();
 }
