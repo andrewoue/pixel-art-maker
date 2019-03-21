f3fa7880ad89431935cf4c87b28559fa11d2bdba
Andrew Ouellette <ouellette.andrew@gmail.com>
2019-03-21 13:08:58 -0500

    Added Make Grid function

--- designs.js
+++ designs.js
@@ -1,21 +1,29 @@
+/*jshint esversion: 6 */
+
 // Select color input
 console.log('Check');
 console.log($('#colorPicker').val());
 
-// Select size input
-console.log($('#inputHeight').val());
-console.log($('#inputWidth').val());
+// Constants
+//const inputHeight = $('#inputHeight').val();
+//const inputWidth = $('#inputWidth').val();
 
 // Grid size button
 $('#gridSizeButton').on('click', function (event) {
   event.preventDefault();
-  console.log('Listening');
+  const height = $('#inputHeight').val();
+  const width = $('#inputWidth').val();
+  makeGrid(height, width);
 });
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-
-// Your code goes here!
-
+function makeGrid(height, width) {
+  for (let h = 1; h <= height; h++) {
+    $('#pixelCanvas').append('<tr>');
+    console.log('row' + h);
+    for (let w = 1; w <= width; w++) {
+      $('tr:last-child').append('<td>');
+    }
+  }
 }
