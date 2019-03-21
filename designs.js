7480d29391a2b70dc32d2633235e0abe72c856b7
Andrew Ouellette <ouellette.andrew@gmail.com>
2019-03-21 11:48:53 -0500

    Event listener added for grid size button

--- designs.js
+++ designs.js
@@ -1,5 +1,16 @@
 // Select color input
+console.log('Check');
+console.log($('#colorPicker').val());
+
 // Select size input
+console.log($('#inputHeight').val());
+console.log($('#inputWidth').val());
+
+// Grid size button
+$('#gridSizeButton').on('click', function (event) {
+  event.preventDefault();
+  console.log('Listening');
+});
 
 // When size is submitted by the user, call makeGrid()
 
